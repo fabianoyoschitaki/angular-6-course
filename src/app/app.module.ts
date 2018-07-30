0f9e40a7d8da413942668f2273a74f1c8d2cf15e
Fabiano <fabianoyoschitaki@gmail.com>
2018-07-30 17:52:14 -0300

    assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
