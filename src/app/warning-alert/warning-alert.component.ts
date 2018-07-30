db0c05984caf855af621308200132b9325e75cb9
Fabiano <fabianoyoschitaki@gmail.com>
2018-07-30 18:01:20 -0300

    alerts

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,6 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-warning-alert',
+    styles: [
+        `
+            p {
+                padding: 20px;
+                background-color: mistyrose;
+                border: 1px solid red;
+            }
+        `
+    ],
     template: `
         <p>This is a warning, you are in danger!</p>
     `
